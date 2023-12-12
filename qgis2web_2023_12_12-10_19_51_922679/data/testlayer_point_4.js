511adc529402fd8c3ed05e0ab9147d6967cae379
smiddel <153201373+smiddel@users.noreply.github.com>
2023-12-12 10:33:28 +0100

    Add files via upload

---
+++ qgis2web_2023_12_12-10_19_51_922679/data/testlayer_point_4.js
@@ -0,0 +1,1 @@
+var json_testlayer_point_4 = {"type":"FeatureCollection","name":"testlayer_point_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[7.137106279597448,50.278193229061408]}}]}
